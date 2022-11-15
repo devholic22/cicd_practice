3bd6df550f3373d21d58e4f5bb7a8cddacd12603
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2022-11-16 01:38:04 +0900

    initial commit

---
+++ src/test/java/solo/devholic/cicd_practice/CicdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package solo.devholic.cicd_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
